3d3f2982f5ca5b36b8dcb4a538945a44d4d42eb4
sultonoir <sultonalidrus5@gmail.com>
2023-08-31 19:05:06 +0700

    first commit

---
+++ app/account/page.tsx
@@ -0,0 +1,16 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { Database } from "@/types";
+import AccountForm from "./account-form";
+
+export default async function Account() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return (
+    <div>{session ? <AccountForm session={session} /> : <p>no data</p>}</div>
+  );
+}
